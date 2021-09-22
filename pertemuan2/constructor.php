0cf474880aade7ec1e160eabdd689e6c0998f826
RobyFathulHabibie20 <193040102.roby@mail.unpas.ac.id>
2021-09-22 15:37:11 +0700

    Mengubah file produk.php menjadi constructor.php dan menambahkan metode constructor pada isi file tersebut

---
+++ pertemuan2/constructor.php
@@ -0,0 +1,29 @@
+<?php
+
+class Produk{
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+
+$produk1 = new Produk("OnePiece", "Eiichiro Oda", "Tafarglaw D. Water Law", 50000);
+$produk2 = new Produk("GTAV", "Ucok", "Rockstars", 150000);
+$produk3 = new Produk("Smackdown Pain");
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
